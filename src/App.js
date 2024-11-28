b98f46b1bfa89de18d45f5a8c58893d0f5710c45
JaneEND32 <dubikovaevgenia32@gmail.com>
2024-11-28 10:26:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект";
+  const greeting = "Это тестовый проект Дубиковой Е. Г.";
   return (
     <div className="App">
      <header className="App-header">
